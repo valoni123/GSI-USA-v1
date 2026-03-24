967b17494e9937f80ec18790a32d5895ea0e3835
[dyad] <valon.kajtazaj@gac-it.at>
2026-03-24 17:22:20 +0100

    [dyad] Added route-level GSI permission guards in App.tsx so direct URL access is blocked and unauthorized users are redirected to /menu based on stored gsi_users flags, with admin override still handled by hasPermission.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import {
+  canAccessIncomingMenu,
+  canAccessInfoStockMenu,
+  canAccessOutgoingMenu,
+  canAccessTransportMenus,
+  getStoredGsiPermissions,
+  hasPermission,
+  type GsiPermissions,
+} from "@/lib/gsi-permissions";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Menu from "./pages/Menu";
@@ -13,7 +22,6 @@ import IncomingGoodsReceipt from "./pages/IncomingGoodsReceipt";
 import IncomingInspection from "./pages/incominginspection";
 import InfoStockMenu from "./pages/InfoStockMenu";
 import InfoStockArticle from "./pages/InfoStockArticle";
-
 import InfoStockTransfer from "./pages/InfoStockTransfer";
 import InfoStockCorrection from "./pages/InfoStockCorrection";
 import TransportMenu from "./pages/TransportMenu";
@@ -46,6 +54,17 @@ const ProtectedRoute = () => {
   return <Outlet />;
 };
 
+const PermissionRoute = ({ allow }: { allow: (permissions: GsiPermissions) => boolean }) => {
+  const location = useLocation();
+  const permissions = getStoredGsiPermissions();
+
+  if (!allow(permissions)) {
+    return <Navigate to="/menu" replace state={{ from: location.pathname }} />;
+  }
+
+  return <Outlet />;
+};
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
@@ -58,24 +77,53 @@ const App = () => (
 
           <Route element={<ProtectedRoute />}>
             <Route path="/menu" element={<Menu />} />
-            <Route path="/menu/incoming" element={<IncomingMenu />} />
-            <Route path="/menu/outgoing" element={<OutgoingMenu />} />
-            <Route path="/menu/outgoing/picking" element={<OutgoingPicking />} />
-            <Route path="/menu/incoming/goods-receipt" element={<IncomingGoodsReceipt />} />
-            <Route path="/menu/incoming/inspection" element={<IncomingInspection />} />
 
-            <Route path="/menu/info-stock" element={<InfoStockMenu />} />
-            <Route path="/menu/info-stock/article" element={<InfoStockArticle />} />
-            <Route path="/menu/info-stock/transfer" element={<InfoStockTransfer />} />
-            <Route path="/menu/info-stock/correction" element={<InfoStockCorrection />} />
-            <Route path="/menu/info-stock/le-info" element={<InfoStockLEInfo />} />
-            <Route path="/menu/transport" element={<TransportMenu />} />
-            <Route path="/menu/transport/load" element={<TransportLoad />} />
-            <Route path="/menu/transport/unload" element={<TransportUnload />} />
-            <Route path="/menu/transports" element={<TransportsSelect />} />
-            <Route path="/menu/transports/list" element={<TransportsList />} />
-            <Route path="/transport/select" element={<TransportSelect />} />
-            <Route path="/transportgroup/:group" element={<TransportGroup />} />
+            <Route element={<PermissionRoute allow={canAccessIncomingMenu} />}>
+              <Route path="/menu/incoming" element={<IncomingMenu />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "rece")} />}>
+              <Route path="/menu/incoming/goods-receipt" element={<IncomingGoodsReceipt />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "insp")} />}>
+              <Route path="/menu/incoming/inspection" element={<IncomingInspection />} />
+            </Route>
+
+            <Route element={<PermissionRoute allow={canAccessOutgoingMenu} />}>
+              <Route path="/menu/outgoing" element={<OutgoingMenu />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "pick")} />}>
+              <Route path="/menu/outgoing/picking" element={<OutgoingPicking />} />
+            </Route>
+
+            <Route element={<PermissionRoute allow={canAccessInfoStockMenu} />}>
+              <Route path="/menu/info-stock" element={<InfoStockMenu />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "itif")} />}>
+              <Route path="/menu/info-stock/article" element={<InfoStockArticle />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "trans")} />}>
+              <Route path="/menu/info-stock/transfer" element={<InfoStockTransfer />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "corr")} />}>
+              <Route path="/menu/info-stock/correction" element={<InfoStockCorrection />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "huif")} />}>
+              <Route path="/menu/info-stock/le-info" element={<InfoStockLEInfo />} />
+            </Route>
+
+            <Route element={<PermissionRoute allow={canAccessTransportMenus} />}>
+              <Route path="/menu/transport" element={<TransportMenu />} />
+              <Route path="/menu/transports" element={<TransportsSelect />} />
+              <Route path="/menu/transports/list" element={<TransportsList />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "trlo")} />}>
+              <Route path="/menu/transport/load" element={<TransportLoad />} />
+              <Route path="/transport/select" element={<TransportSelect />} />
+              <Route path="/transportgroup/:group" element={<TransportGroup />} />
+            </Route>
+            <Route element={<PermissionRoute allow={(permissions) => hasPermission(permissions, "trul")} />}>
+              <Route path="/menu/transport/unload" element={<TransportUnload />} />
+            </Route>
           </Route>
 
           <Route path="*" element={<NotFound />} />
